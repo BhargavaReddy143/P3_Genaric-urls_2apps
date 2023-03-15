f32f93e4cd14e1386de407868ad521558baa092a
BhargavaReddy143 <bannybhe@gmail.com>
2023-03-15 16:03:22 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def app2(request):
+    return HttpResponse("Hello App2 Moowa!!!!!")
+def B(request):
+    return HttpResponse("Hello B Broooo!!!!!!!")    
